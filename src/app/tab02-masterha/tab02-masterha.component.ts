cf2db9fe74ad847b82d8d0a72d02b87cb75f9249
masterha <masterha@sheridancollege.ca>
2024-08-12 12:48:27 -0400

    tab02 done

--- src/app/tab02-masterha/tab02-masterha.component.ts
+++ src/app/tab02-masterha/tab02-masterha.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Classmasterha } from '../app.component';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-tab02-masterha',
   templateUrl: './tab02-masterha.component.html',
   styleUrl: './tab02-masterha.component.css'
 })
 export class Tab02MasterhaComponent {
-  @Input() masterhaS2024!: Classmasterha;  // Receive the Classmasterha instance from the parent
+  @Input() masterhaS2024!: Classmasterha;
+  masterhaForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.masterhaForm = this.fb.group({
+      masterhamonth66464: ['August'],
+      masterhayear66464: ['2024']
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.masterhaForm.value);
+  }
 }
